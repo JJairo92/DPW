40c5155f7746c2ab51776bc77cfc1038cd73808a
Jairo <JJEcu13@hotmail.com>
2014-01-08 14:36:50 -0500

    changed equation to velocity by asking for time and distance

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -13,14 +13,18 @@ if gender == "male" or gender == "Male":
 else:
 	genderPronoun = "She"
 
-# 
+# Velocity and Distance
+distance = raw_input("To find the velocity of something traveled, we need the distance. What is the distance? ")
+time = raw_input("Now, what is the time of travel? ")
+
+def calcVelocity
 
 
 # Madlib
 madlib = '''I cannot believe how good of a friend {name} is. {genderPronoun} helped me with 
 my physics homework. {genderPronoun} showed me the equation to find the Gravitational Force.
 The equation is F8 = G(m1*m2/d^2); G being the gravitational constant, m1 being mass 1, m2
-being mass 2, and d being distance.'''
+being mass 2, and d being distance. '''
 madlibFormatted = madlib.format(**locals())
 
 print madlibFormatted
