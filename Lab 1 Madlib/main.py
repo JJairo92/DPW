a3a48203bcac5b3a1d147883d391214b5d3cb404
Jairo <JJEcu13@hotmail.com>
2014-01-08 11:19:45 -0500

    first input appearing in formatted message

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -3,3 +3,11 @@
 # 1/6/2014
 # Madlib Lab
 
+# Name
+name = raw_input("Please type your name: ")
+
+# Madlib
+message = '''I cannot believe how good of a friend {name} is.'''
+messageFormatted = message.format(**locals())
+
+print messageFormatted
