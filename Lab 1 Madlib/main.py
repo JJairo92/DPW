1cf8a533fcc1e97ef938ca72da4d3fe290cca86e
Jairo <JJEcu13@hotmail.com>
2014-01-08 11:20:58 -0500

    added gender conditional

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -6,8 +6,17 @@
 # Name
 name = raw_input("Please type your name: ")
 
+# Male/Female and the conditional for pronouns
+gender = raw_input("Are you a male or a female? ")
+if gender == "male" or gender == "Male":
+	genderPronoun = "He"
+else:
+	genderPronoun = "She"
+
+
 # Madlib
-message = '''I cannot believe how good of a friend {name} is.'''
+message = '''I cannot believe how good of a friend {name} is. {genderPronoun} helped me with 
+my homework.'''
 messageFormatted = message.format(**locals())
 
 print messageFormatted
