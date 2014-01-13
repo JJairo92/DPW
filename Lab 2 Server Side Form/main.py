297d0e3ca6931df43a73b0a8ed46e55950219da9
Jairo <JJEcu13@hotmail.com>
2014-01-13 12:55:03 -0500

    added classes to input elements

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -16,20 +16,20 @@ class MainHandler(webapp2.RequestHandler):
     	form = '''
     	<form method="GET">
     		<label for="name">Name: </label>
-    		<input type="text" placeholder="John Smith" /><br />
+    		<input class="input" type="text" placeholder="John Smith" /><br />
 
     		<label for="email">Email Address: </label>
-    		<input type="email" placeholder="johnsmith@email.com" name="email"/><br />
+    		<input class="input" type="email" placeholder="johnsmith@email.com" name="email"/><br />
 
-    		<label for="system">What system information do you prefer?</label>
-			<select name="system" id="system">
+    		<label for="system">What system information do you prefer?</label><br />
+			<select class="input" name="system" id="system">
 				<option>Console</option>
 				<option>PC</option>
 				<option>Mobile</option>
-			</select>
+			</select><br />
 
     		<label for="agreement">Agreement:</label>
-			<input type="checkbox" name="agreement" id="agreement" required="required" />I have read and agree to the <a href="#">User Agreement</a>.
+			<input class="input" type="checkbox" name="agreement" id="agreement" required="required" />I agree to sign-up for this newsletter.<br />
     		<input type="submit" value="Send" />
     	</form>
     	'''
