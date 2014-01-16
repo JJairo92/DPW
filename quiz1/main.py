d42604ec7bc4a9f5ba1ba8d74d9809b812151c87
Jairo <JJEcu13@hotmail.com>
2014-01-15 22:49:19 -0500

    finished formatted message and completed area function part of quiz

--- quiz1/main.py
+++ quiz1/main.py
@@ -5,7 +5,7 @@
 
 # Area Calculation
 width = 5
-height = 5
+height = 7
 shape = ''
 
 # Conditional for determining whether shape is a rectangle or a square
@@ -19,3 +19,8 @@ def calcArea(w, h):
 	area = w * h
 	return area
 a = calcArea(width, height)
+
+message = '''The area of your {shape} is {a} square feet.'''
+messageFormatted = message.format(**locals())
+
+print messageFormatted
