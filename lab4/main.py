12ce8b80646973561468547779589a511bcadbb7
Jairo <JJEcu13@hotmail.com>
2014-01-20 13:20:16 -0500

    added css link, as well as title, and created the main page class in main.py

--- lab4/main.py
+++ lab4/main.py
@@ -7,7 +7,13 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page = PageBase()
+        page.title = "Animal Classes"
+        page.css = '<link rel="stylesheet" type="text/css" href="css/main.css"  />'
+
+    	self.response.write(page.header())
+
+    	self.response.write(page.footer())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
@@ -15,4 +21,25 @@ app = webapp2.WSGIApplication([
 
 class PageBase(object):
 	def __init__(self):
-		self.title
+		self._header = '''<!DOCTYPE>
+	<html lang="en">
+		<head>
+			<title>{self.title}</title>
+			{self.css}
+		</head>
+		<body>
+			<h1>Animal Classes</h1>'''
+
+		self._footer = '''
+		</body>
+
+		<footer>
+			<p>Copyright &copy; 2014 <strong>Animal Classes</strong></p>
+		</footer>
+	</html>'''
+
+	def header(self):
+		return self._header
+
+	def footer(self):
+		return self._footer
