b2cf5f18c61306e57c2d34c5617f8ec4e47a8f47
Jairo <JJEcu13@hotmail.com>
2014-01-27 21:50:48 -0500

    changed xml to import json and added model to grab artist from input

--- lab5-api/main.py
+++ lab5-api/main.py
@@ -6,11 +6,15 @@
 import webapp2
 from page import *
 import urllib2
-from xml.dom import minidom
+import json
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
+		if self.request.GET:
+			artist = self.request.GET['artist']
+			xmldoc = minidom.parse(result)
 		page = Page()
+		# form_settings =
 		page.update()
 
 		self.response.write(page.header + page.footer)
@@ -18,3 +22,11 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
 ], debug=True)
+
+class Model(object):
+	def __init__(self, artist):
+		self.__url = "http://api.bandsintown.com/artists/"+artist+".json?app_id=artistElookup"
+		self.__req = urllib2.Request(self.__url)
+		self.__opener = urllib2.build_opener()
+		self.__result = self.__opener.open(self.__req)
+		self.__obj = json.load(self.__result)
