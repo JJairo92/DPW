e918647625c3b49d843b413030536ece7fe1642f
Jairo <JJEcu13@hotmail.com>
2014-01-29 16:34:35 -0500

    added dates and links to page as well as comments to page.py

--- lab5-api/page.py
+++ lab5-api/page.py
@@ -1,13 +1,19 @@
+# Class that holds what and how it will be printed to the page
 class ArtistView(object):
 	def __init__(self, do):
+		# Artist Name
 		self.__content = '<h2>' + do.artist + ' Concerts</h2>'
+		# Each information that was looped through
 		for event in do.events:
 			self.__content += '<div class="event"><div class="venues"><h3>Venue</h3><p>'+event[0]+'</p></div>' + '<div class="cities"><h3>City</h3><p>'+event[1]+'</p></div>' + '<div class="regions"><h3>State/Region</h3><p>'+event[2]+'</p></div>' + '<div class="countries"><h3>Country</h3><p>'+event[3]+'</p></div></div>'
+		for dateLink in do.datesLinks:
+			self.__content += '<div class="button"><h3>Date</h3><p>'+dateLink[0]+'</p>' + '<h3><Link</h3><a href="'+dateLink[1]+'" target="_blank"><button>Find Tickets!</button></a></div>'
 
 	@property
 	def content(self):
 		return self.__content
 
+# Page class that holds basic html code
 class Page(object):
 	def __init__(self):
 		self.title = "Artist E.Lookup"
@@ -35,7 +41,7 @@ class Page(object):
 	def footer(self):
 		return self.__footer
 	
-
+# subclass to Page class, it holds the input field to search for artist
 class Form(Page):
 	def __init__(self, obj):
 		super(Form, self).__init__()
@@ -56,6 +62,7 @@ class Form(Page):
 	def getForm(self):
 		return self.__formOpen + self.__inputs + self.__formClose
 
+	# update function/method to update page, this is called
 	def update(self):
 		self._header = self._header.format(**locals())
 		self.__formOpen = self.__formOpen.format(**locals())
