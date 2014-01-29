2f8f80b011a0e03f75884b9953c67c6836be9747
Jairo <JJEcu13@hotmail.com>
2014-01-29 10:53:52 -0500

    added divs to self.__content, also added google fonts link as well as the h1 of the page

--- lab5-api/page.py
+++ lab5-api/page.py
@@ -2,7 +2,7 @@ class ArtistView(object):
 	def __init__(self, do):
 		self.__content = '<h2>' + do.artist + '</h2>'
 		for event in do.events:
-			self.__content += '<h3>Venue: </h3><p>'+event[0]+'</p>' + '<h3>City: </h3><p>'+event[1]+'</p>' + '<h3>Region: </h3><p>'+event[2]+'</p>' + '<h3>Country: </h3><p>'+event[3]+'</p>'
+			self.__content += '<div class="event"><div class="venues"><h3>Venue</h3><p>'+event[0]+'</p></div>' + '<div class="cities"><h3>City</h3><p>'+event[1]+'</p></div>' + '<div class="regions"><h3>State/Region</h3><p>'+event[2]+'</p></div>' + '<div class="countries"><h3>Country</h3><p>'+event[3]+'</p></div></div>'
 
 	@property
 	def content(self):
@@ -18,8 +18,10 @@ class Page(object):
 		<meta charset="utf-8">
 		<title>{self.title}</title>
 		{self.css}
+		<link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,700italic|Rock+Salt' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
+		<h1>Artist E. Lookup</h1>
 '''
 		self.__footer = '''
 	</body>
@@ -45,7 +47,7 @@ class Form(Page):
 
 		self.__inputs = ''
 		for el in obj: # "el" for element in object(obj)
-			self.__inputs += '<input type="{el[type]}" value="{el[label]}" name="{el[name]}"></input>'
+			self.__inputs += '<div id="search-box"><input type="{el[type]}" value="{el[label]}" name="{el[name]}"></input></div>'
 			self.__inputs = self.__inputs.format(**locals())
 
 		self.__formClose = "</form>"
