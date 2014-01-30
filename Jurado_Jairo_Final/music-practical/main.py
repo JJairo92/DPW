2f7ca5ae0eb4b152b52b4ed836af83502172ad58
Jairo <JJEcu13@hotmail.com>
2014-01-29 21:17:24 -0500

    added music player; buttons are populated now

--- Jurado_Jairo_Final/music-practical/main.py
+++ Jurado_Jairo_Final/music-practical/main.py
@@ -12,31 +12,50 @@ class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		page = Page()
 		page.update()
-		self.response.write(page.header + page.footer)
 
-class MusicModel(object):
-	def __init__(self):
-		self.__url = "Music.xml"
-		self.__req = urllib2.Request(self.__url)
-		self.__opener = urllib2.build_opener()
-		self.send()
+		form_settings = [{"song":"Like a Rolling Stone"},{"song":"Satisfaction"}]
+		form = Form(form_settings)
+		form.update()
 
-	def send(self):
-		self.__result = self.__opener.open(self.__req)
-		self.sort()
+		self.response.write(page.header + form.getForm + page.footer)
 
-	def sort(self):
-		self.__xmldoc = minidom.parse(self.__result)
-		
+		# if self.request.GET['button']:
+		# 	model = MusicModel(self.request.GET['button'])
+		# 	view = MusicView(model.content)
+
+		# 	self.response.write(view.content)
+
+# class MusicModel(object):
+# 	def __init__(self):
+# 		self.__url = "Music.xml"
+# 		self.__req = urllib2.Request(self.__url)
+# 		self.__opener = urllib2.build_opener()
+# 		self.send()
+
+# 	def send(self):
+# 		self.__result = self.__opener.open(self.__req)
+# 		self.sort()
+
+# 	def sort(self):
+# 		self.__xmldoc = minidom.parse(self.__result)
+# 		self.__do = TrackData()
+# 		songs = self.__xmldoc.getElementsByTagName('track')
+# 		for song in songs:
+# 			songsDict = dict()
+# 			title = song.getElementsByTagName('title')[0].firstChild.nodeValue
+# 			artist = song.getElementsByTagName('artist')[0].firstChild.nodeValue
+# 			length = song.getElementsByTagName('length')[0].firstChild.nodeValue
+# 			year = song.getElementsByTagName('year')[0].firstChild.nodeValue
+# 			label = song.getElementsByTagName('label')[0].firstChild.nodeValue
+
+# 			songsDict = [title]
+# 			self.__do.songs.append(songsDict)
+# 		print self.__do.songs
 
 
 class TrackData(object):
 	def __init__(self):
-		self.name = ''
-		self.artist = ''
-		self.length = ''
-		self.year = ''
-		self.label = ''
+		self.songs = []
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
--- Jurado_Jairo_Final/music-practical/page.py
+++ Jurado_Jairo_Final/music-practical/page.py
@@ -1,6 +1,11 @@
-class View(object):
-	def __init__(self):
-		self.__content = ''
+class MusicView(object):
+	def __init__(self, do):
+		for song in do.songs:
+			self.__content = '<h3>' +song[0]+ '</h3>'
+
+	@property
+	def content(self):
+		return self.__content
 
 class Page(object):
 	def __init__(self):
@@ -12,7 +17,20 @@ class Page(object):
 				<title>{self.title}</title>
 			</head>
 			<body>
-				<h1>Music App</h1>'''
+				<h1>Music App</h1>
+				<ul>
+					<li>Good Vibrations</li>
+					<audio src="mp3s/Good_Vibrations.mp3" controls="controls"></audio><br />
+					<li>Hey Jude</li>
+					<audio src="mp3s/Hey-Jude.mp3" controls="controls"></audio>
+					<li>Imagine</li>
+					<audio src="mp3s/imagine.mp3" controls="controls"></audio>
+					<li>Rolling Stone</li>
+					<audio src="mp3s/rollingstone.mp3" controls="controls"></audio>
+					<li>Smells Like Teen Spirit</li>
+					<audio src="mp3s/smells-like-teen-spirit.mp3" controls="controls"></audio>
+					<li>What's Going On</li>
+					<audio src="mp3s/WhatsGoingOn.mp3" controls="controls"></audio></ul>'''
 
 		self.__footer = '''
 			</body>
@@ -28,3 +46,28 @@ class Page(object):
 
 	def update(self):
 		self._header = self._header.format(**locals())
+
+
+class Form(Page):
+	def __init__(self, obj):
+		super(Form, self).__init__()
+
+		self.method = 'GET'
+		self.action = ''
+
+		self.__formOpen = '''
+		<form action="{self.action}" method="{self.method}">'''
+		self.__links = ''
+		for el in obj:
+			self.__links += '<button>{el[song]}</button>'
+			self.__links = self.__links.format(**locals())
+
+		self.__formClose = '</form>'
+
+	@property
+	def getForm(self):
+		return self.__formOpen + self.__links + self.__formClose
+
+	def update(self):
+		self._header = self._header.format(**locals())
+		self.__formOpen = self.__formOpen.format(**locals())
