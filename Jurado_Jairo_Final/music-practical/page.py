05cd5548dc4168bba3ddbe97b1f471e0594624f5
Jairo <JJEcu13@hotmail.com>
2014-01-29 19:48:20 -0500

    created page class with basic html, added header property

--- Jurado_Jairo_Final/music-practical/page.py
+++ Jurado_Jairo_Final/music-practical/page.py
@@ -1,3 +1,23 @@
 class View(object):
-	def __init__(self, do):
+	def __init__(self):
 		self.__content = ''
+
+class Page(object):
+	def __init__(self):
+		self.title = "Music App"
+		self._header = '''<!DOCTYPE>
+		<html>
+			<head>
+				<meta charset="utf-8">
+				<title>{self.title}</title>
+			</head>
+			<body>
+				<h1>Music App</h1>'''
+
+		self.__footer = '''
+			</body>
+		</html>'''
+
+	@property
+	def header(self):
+		return self._header
