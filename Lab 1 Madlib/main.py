f0e821611b2d5488e3ccc2c19b3dcc8dcf9b89bd
Jairo <JJEcu13@hotmail.com>
2014-03-06 13:07:44 -0500

    added boolean conditional

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -7,7 +7,7 @@
 strings = []
 
 # User's Name
-name = raw_input("Enter your full name: ")
+name = raw_input("Enter your full name: ") # "raw_input" is for strings
 
 # Calculate age input variable and function
 year_curr = 2014
@@ -17,11 +17,21 @@ def calc_age(yc, yb):
 	a = yc - yb
 	return a
 age = calc_age(year_curr, year_born)
+strings.append(age)
+
+# Conditional to check legal drinking age
+legal_drinker = False # "True" if legal drinker; the conditional will determine it
+if age >= 21:
+	legal_drinker = True
+else:
+	legal_drinker = False
+
+# Calculate
 
 print age
 
 # Madlib
-madlib = '''Test madlib'''
+madlib = '''I met'''
 
 madlib = madlib.format(**locals())
 print madlib
