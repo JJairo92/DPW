024fc3c480ae364c44171a468d62b4463666fbf8
Jairo <JJEcu13@hotmail.com>
2014-03-06 12:53:24 -0500

    added first function; works fine

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -3,9 +3,22 @@
 # DPW
 # Lab 1 - Madlib
 
+# Array to hold strings
+strings = []
+
 # User's Name
 name = raw_input("Enter your full name: ")
-print name
+
+# Calculate age input variable and function
+year_curr = 2014
+year_born = input("In what year where you born? ") # "input" accepts integers
+
+def calc_age(yc, yb):
+	a = yc - yb
+	return a
+age = calc_age(year_curr, year_born)
+
+print age
 
 # Madlib
 madlib = '''Test madlib'''
