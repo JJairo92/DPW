87cd06d78914dc02580963c87de029545d4bf07b
Jairo <JJEcu13@hotmail.com>
2014-03-06 13:36:33 -0500

    added array and dictionary

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -5,10 +5,10 @@
 
 # Array to hold strings
 strings = []
-numbers = dict()
 
 # User's Name
 name = raw_input("Enter person's full name: ") # "raw_input" is for strings
+
 
 # Gender; Pronoun function
 gender = raw_input("Enter person's gender: ")
@@ -17,23 +17,31 @@ if gender == "male" or gender == "Male":
 else:
 	gender_pronoun = "She"
 
+
+
 # Calculate age input variable and function
 year_curr = 2014
 year_born = input("Enter the year the person was born: ") # "input" accepts integers
 
 def calc_age(yc, yb):
-	a = yc - yb
-	return a
-age = calc_age(year_curr, year_born)
+	age = yc - yb
+	return age
+a = calc_age(year_curr, year_born)
 
 # Conditional to check legal drinking age
-if age >= 21:
+if a >= 21:
 	legal_drinker = True
 else:
 	legal_drinker = False
 
+# Dictionary to hold numbers
+numbers = dict()
+numbers = dict(age=a)
+print numbers
+print strings
+
 # Madlib
-madlib = '''I met an interesting person last night. '''
+madlib = '''I met an interesting person last night. {strings[1]}'''
 
 madlib = madlib.format(**locals())
 print madlib
