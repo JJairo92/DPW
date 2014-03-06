99375cbd1cccbfbbe0fedd164d282385e8482ae7
Jairo <JJEcu13@hotmail.com>
2014-03-06 14:32:15 -0500

    added estimated sales function; added numbers to numbers dictionary

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -7,41 +7,56 @@
 strings = []
 
 # User's Name
-name = raw_input("Enter person's full name: ") # "raw_input" is for strings
-strings.append(name)
+name = raw_input("Enter artist's stage name: ") # "raw_input" is for strings
+strings.append(name) # adds "name" to strings array
 
 # Gender; Pronoun function
-gender = raw_input("Enter person's gender: ")
+gender = raw_input("Enter artist's gender: ")
 if gender == "male" or gender == "Male":
 	gender_pronoun = "He"
 else:
 	gender_pronoun = "She"
-strings.append(gender_pronoun)
+strings.append(gender_pronoun) # adds "gender_pronoun" to strings array
 
-
-# Calculate age input variable and function
+# Age function
 year_curr = 2014
-year_born = input("Enter the year the person was born: ") # "input" accepts integers
+year_born = input("Enter the year the artist was born: ") # "input" accepts integers
 
 def calc_age(yc, yb):
 	age = yc - yb
 	return age
 a = calc_age(year_curr, year_born)
 
-# Conditional to check legal drinking age
-if a >= 21:
-	legal_drinker = True
+# Sales Estimate function
+sales = input("Enter current album sales for artist: ")
+popularity = raw_input("Does this person pass our popularity standards? (y/n) ")
+
+if popularity == 'y' or popularity == 'Y':
+	pop_bool = True
 else:
-	legal_drinker = False
+	pop_bool = False
+
+def calc_future_sales(sales, pop_bool):
+	hispanic_states = 5
+	usual_sales = 10000
+
+	if pop_bool == True:
+		pop_bonus = 20000
+	else:
+		pop_bonus = 0
+
+	estimated_sales = sales + (usual_sales * 5) + pop_bonus
+	return estimated_sales
+est_sales = calc_future_sales(sales, pop_bool)
 
 # Dictionary to hold numbers
 numbers = dict()
-numbers = dict(age=a)
+numbers = dict(age=a,future_sales=est_sales,curr_sales=sales)
 print numbers
 print strings
 
 # Madlib
-madlib = '''I met an interesting person last night. {strings[1]}'''
+madlib = '''I met an interesting person last night. {strings[1]} likes to be called {strings[0]} on stage. {strings[1]} is a singer and very well known in the hispanic community. We would like to get him to join our record company so he can reach the American community. We estimate '''
 
 madlib = madlib.format(**locals())
 print madlib
