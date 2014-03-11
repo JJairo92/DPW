ad59c3c175dc3a3c0eebd83268c5f9e5efada493
Jairo <JJEcu13@hotmail.com>
2014-03-11 15:37:30 -0400

    finished result "get" conditional, as well as the css for it

--- lab2/main.py
+++ lab2/main.py
@@ -10,7 +10,31 @@ class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		page = Page()
 
-		self.response.write(page.header + page.form + page.footer)
+		if self.request.GET:
+			first_name = self.request.GET['first-name']
+			last_name = self.request.GET['last-name']
+			email = self.request.GET['email']
+			news_info = self.request.GET['system']
+
+			result = '''<div id="results">
+				<p>Thank you for signing up!</p>
+
+				<p class="labels">First Name:</p>
+				<p class="labels">Last Name:</p>
+				<p class="labels">Email:</p>
+				<p class="labels">Info you'd like to receive:</p>
+
+				<p class="inputs">{first_name}</p>
+				<p class="inputs">{last_name}</p>
+				<p class="inputs">{email}</p>
+				<p class="inputs">{news_info}</p>
+			</div>'''
+
+			result = result.format(**locals())
+
+			self.response.write(page.header + result + page.footer)
+		else:
+			self.response.write(page.header + page.form + page.footer)
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
