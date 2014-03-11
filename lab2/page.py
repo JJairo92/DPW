1e33a86be417186ad5e2833c1a746503746e6928
Jairo <JJEcu13@hotmail.com>
2014-03-11 14:00:21 -0400

    added page.py file with header plus header method

---
+++ lab2/page.py
@@ -0,0 +1,12 @@
+class Page():
+	def __init__(self):
+		self.header = '''<!DOCTYPE>
+	<html lang="en">
+		<head>
+			<title>Gamer Addictive | Newsletter Sign-Up</title>
+		</head>
+		<body>
+			<h1>Gamer Addictive Newsletter Sign-Up</h1>'''
+
+	def header(self):
+		return self.header
