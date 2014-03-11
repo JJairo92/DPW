dfb9287d2633994b21b39afdc40735bbb0262a53
Jairo <JJEcu13@hotmail.com>
2014-03-11 14:12:53 -0400

    added google font link plus meta charset to head in page.py header attribute

--- lab2/page.py
+++ lab2/page.py
@@ -3,8 +3,10 @@ class Page():
 		self.header = '''<!DOCTYPE>
 	<html lang="en">
 		<head>
+			<meta charset="utf-8">
 			<title>Gamer Addictive | Newsletter Sign-Up</title>
 			<link rel="stylesheet" href="css/main.css" type="text/css" />
+			<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:400,600' rel='stylesheet' type='text/css'>
 		</head>
 		<body>
 			<h1>Gamer Addictive Newsletter Sign-Up</h1>'''
