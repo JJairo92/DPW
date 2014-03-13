2e9aa94b53245ec41198d30dd7bb712295585ad1
Jairo <JJEcu13@hotmail.com>
2014-03-13 12:52:59 -0400

    added Tomb Raider Object with all 6 attributes; added title attribute to Game Class

--- lab3/main.py
+++ lab3/main.py
@@ -7,10 +7,17 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		self.response.write('Hello world!')
+		self.tomb = Game()
+		self.tomb.title = "Tomb Raider (2013)"
+		self.tomb.ps3_sales = 1872139
+		self.tomb.xbox360_sales = 1630789
+		self.tomb.pc_sales = 248657
+		self.tomb.ps4_sales = 293112
+		self.tomb.xboxone_sales = 99107
 
 class Game(object):
 	def __init__(self):
+		self.title = ""
 		self.ps3_sales = 0
 		self.xbox360_sales = 0
 		self.pc_sales = 0
