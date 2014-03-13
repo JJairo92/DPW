2a1d1e79dc2ecc0ad6350d0cf2a8c5a6888cd712
Jairo <JJEcu13@hotmail.com>
2014-03-13 13:01:45 -0400

    added ac4 data object with all attributes

--- lab3/main.py
+++ lab3/main.py
@@ -7,6 +7,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
+		# Tomb Raider (2013)
 		self.tomb = Game()
 		self.tomb.title = "Tomb Raider (2013)"
 		self.tomb.ps3_sales = 1872139
@@ -15,6 +16,16 @@ class MainHandler(webapp2.RequestHandler):
 		self.tomb.ps4_sales = 293112
 		self.tomb.xboxone_sales = 99107
 
+		# Assassin's Creed IV
+		self.acreed = Game()
+		self.acreed.title = "Assassin's Creed IV: Black Flag"
+		self.acreed.ps3_sales = 2714198
+		self.acreed.xbox360_sales = 2392960
+		self.acreed.pc_sales = 393637
+		self.acreed.ps4_sales = 1434353
+		self.acreed.xboxone_sales = 692308
+
+# Data-Object Class
 class Game(object):
 	def __init__(self):
 		self.title = ""
