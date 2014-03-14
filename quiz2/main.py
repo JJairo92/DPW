4b4af166f18e0a8c85e137228a508119939feae3
Jairo <JJEcu13@hotmail.com>
2014-03-13 22:16:03 -0400

    created second class with 2 attributes

--- quiz2/main.py
+++ quiz2/main.py
@@ -3,6 +3,8 @@
 # 03/13/2014
 # Quiz 2
 
+# The instructions for this quiz are very confusing; I think you wanted to methods and the "def" used in the property will count as one of them, that's why I added one more "def" to class Student
+
 class Student(object):
 	_first_name = 'Jairo'
 	_last_name = 'Jurado'
@@ -14,9 +16,13 @@ class Student(object):
 	def first_name(self):
 		return _first_name
 
-	def calc_total_games(ng, ug):
+	def calc_total_games(ng, ug): # method with 2 parameters
 		total = ng + ug
 		return total
 	total_games = calc_total_games(new_games, used_games)
 
 	print _first_name+ " " +_last_name+ " has a total of " +str(total_games)+ " games."
+
+class House(object):
+	windows = 2
+	rooms = 3
