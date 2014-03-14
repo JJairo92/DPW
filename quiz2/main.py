7a2f917aa5ac140062275de502e7d35756e5e5ed
Jairo <JJEcu13@hotmail.com>
2014-03-13 22:10:43 -0400

    added 2 more attributes to first and created a function since there needs to be 2

--- quiz2/main.py
+++ quiz2/main.py
@@ -7,3 +7,16 @@ class Student(object):
 	_first_name = 'Jairo'
 	_last_name = 'Jurado'
 	username = 'JJairo92'
+	new_games = 4
+	used_games = 2
+
+	@property
+	def first_name(self):
+		return _first_name
+
+	def calc_total_games(ng, ug):
+		total = ng + ug
+		return total
+	total_games = calc_total_games(new_games, used_games)
+
+	print _first_name+ " " +_last_name+ " has a total of " +str(total_games)+ " games."
