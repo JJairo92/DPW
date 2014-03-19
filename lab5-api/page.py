5dd49df5c504a374781ccecd3ab09874e626460c
Jairo <JJEcu13@hotmail.com>
2014-03-18 21:56:36 -0400

    added header to page.py

---
+++ lab5-api/page.py
@@ -0,0 +1,13 @@
+class Page(object):
+	def __init__(self):
+		self.header = '''<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>API | Artist e.Lookup</title>
+			<link rel="stylesheet" href="css/main.css" type="text/css" />
+			<link href='http://fonts.googleapis.com/css?family=Fondamento:400,400italic|Lato' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+			<div id="container">
+			<h1>API</h1>'''
