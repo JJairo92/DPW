f1af021347241d77cf3ce172c1b61d5e288cc2c9
Jairo <JJEcu13@hotmail.com>
2014-03-18 22:19:31 -0400

    importing urllib2 and minidom

--- lab5-api/main.py
+++ lab5-api/main.py
@@ -5,13 +5,15 @@
 
 import webapp2
 from page import Page
+import urllib2
+from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		page = Page() # variable to hold "Page" class form page.py
 
 		# API things
-		
+
 		self.response.write(page.header + page.form + page.footer)
 
 		if self.request.GET:
