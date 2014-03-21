902bb59c96e166d39c9bf36f145b591690b1aec6
Jairo <JJEcu13@hotmail.com>
2014-03-20 21:45:29 -0400

    added Live subclass with 2 additional attributes from VideoGame abstract class

--- quiz3/main.py
+++ quiz3/main.py
@@ -12,11 +12,11 @@ class VideoGame(webapp2.RequestHandler):
 		self._genre = "Genre"
 
 	@property
-	def name(self):
+	def name(self): #method 1 (quiz instructions did not say "in addition to property methods")
 		return self._name
 
 	@property
-	def genre(self):
+	def genre(self): #method 2 (quiz instructions did not say "in addition to property methods")
 		return self._genre
 
 # Subclass 1
@@ -29,8 +29,17 @@ class Kill(VideoGame):
 		self._publisher = "Life+Death"
 
 	@property
-	def developer(self):
+	def developer(self): #property for the subclass
 		return self._developer
+
+# Subclass 2
+class Live(VideoGame):
+	def __init__(self):
+		super(Live, self).__init__()
+		self._name = "Live to Kill"
+		self._genre = "RPG"
+		self._developer = "Trident Souls"
+		self._publisher = "Perfected Forever"
 
 app = webapp2.WSGIApplication([
 	('/', VideoGame)
