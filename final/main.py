a0f9077a381c016acf2ece3505788414fd4fb478
Jairo <JJEcu13@hotmail.com>
2014-03-27 19:37:50 -0400

    added xml imports and 2 functions to the model class

--- final/main.py
+++ final/main.py
@@ -5,16 +5,30 @@
 
 import webapp2
 from page import Page
+import urllib2
+from xml.dom import minidom
 
 class MusicController(webapp2.RequestHandler):
 	def get(self):
 		page = Page()
 
-		self.response.write(page.header + page._footer)
+		self.response.write(page.header)
+
+		self.response.write(page._footer)
 
 class MusicModel(object):
 	def __init__(self):
-		pass
+		self.__url = "http://rebeccacarroll.com/api/music/music.xml"
+		self.__request = urllib2.Request(self.__url)
+		self.__opener = urllib2.build_opener()
+		self.send()
+
+	def send(self):
+		self.__result = self.__opener.open(self.__request)
+		self.sort()
+
+	# def sort(self):
+
 
 class MusicData(object):
 	def __init__(self):
