2c3ca4441806a1005f998160ac6af509a0316192
Jairo <JJEcu13@hotmail.com>
2014-03-27 19:22:06 -0400

    added import of page.py to main.py and writing basic html to page

--- final/main.py
+++ final/main.py
@@ -4,11 +4,14 @@
 # Music Practical
 
 import webapp2
+from page import Page
 
 class MusicController(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+		page = Page()
+
+		self.response.write(page.header + page._footer)
 
 app = webapp2.WSGIApplication([
-    ('/', MusicController)
+	('/', MusicController)
 ], debug=True)
