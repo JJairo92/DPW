a371ad8dd1be87f71e9f1d33834c04c716fa7aee
Jairo <JJEcu13@hotmail.com>
2014-03-27 19:13:58 -0400

    created page.py file with a public header

---
+++ final/page.py
@@ -0,0 +1,11 @@
+class Page(object):
+	def __init__(self):
+		self.header = '''<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>Music Practical</title>
+			<link rel="stylesheet" href="css/main.css" type="text/css" />
+		</head>
+		<body>
+			<h1>Music Practical</h1>'''
