507e90e784e2ea6e8f3034eacb31ac1001eae4cb
Jairo <JJEcu13@hotmail.com>
2014-03-27 19:11:33 -0400

    added css handler and changed MainHandler class name

--- final/main.py
+++ final/main.py
@@ -5,10 +5,10 @@
 
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+class MusicController(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MusicController)
 ], debug=True)
